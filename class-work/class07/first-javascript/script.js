13b5558cc7bffa63e10dbd41f2c3accbc6275753
cvam3 <33642086+cvam3@users.noreply.github.com>
2017-12-11 10:57:15 -0500

    Homework
    Startup Matchmaker
    Final Project .txt file

---
+++ class-work/class07/first-javascript/script.js
@@ -0,0 +1,25 @@
+var product = 10*10
+var quotient = 100/1
+var diffrence = 50-17
+var sum = 50+17
+var name = 'Mike ' + 'Glumac'
+
+console.log(product, quotient, diffrence, sum, name);
+
+function onra () {
+    var city = 'Saigon';
+
+    console.log(`Last Tango In ${city}`);
+    
+}
+
+onra();
+onra();
+onra();
+
+var hello = document.getElementById('hello');
+
+hello.style.color = 'tomato';
+
+ 
+
